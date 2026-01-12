3e9bbf62ea0fcb5ee1889299ec5e5f866dba2aaa
Quirin Brändli <qbraendli@pm.me>
2026-01-12 04:00:30 +0100

    modules: (stylix) remove home-manager config

--- modules/core/stylix/nixos/module.nix
+++ modules/core/stylix/nixos/module.nix
@@ -1,63 +1,66 @@
 { lib, config, pkgs, ... }:
 
+let
+  cfg = config.hm.qnix.core.stylix;
+in
 {
-  config = lib.mkMerge [
-    (lib.mkIf (config.hm.qnix.core.stylix.enable or false) {
-      stylix = let cfg = config.hm.qnix.core.stylix; in {
+  config = lib.mkIf cfg.enable  {
+    stylix = {
+      enable = true;
+
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/${cfg.colorScheme}.yaml";
+
+      override = cfg.colorSchemeOverrides;
+
+      homeManagerIntegration.autoImport = false;
+
+      cursor = {
+        package = cfg.cursor.package;
+        name = cfg.cursor.name;
+        size = cfg.cursor.size;
+      };
+
+      opacity = {
+        applications = cfg.opacity.applications;
+        terminal = cfg.opacity.terminal;
+      };
+        
+      icons = {
         enable = true;
 
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/${cfg.colorScheme}.yaml";
+        package = cfg.icons.package;
+        dark = cfg.icons.dark;
+        light = cfg.icons.light;
+      };
 
-        override = cfg.colorSchemeOverrides;
-
-        cursor = {
-          package = cfg.cursor.package;
-          name = cfg.cursor.name;
-          size = cfg.cursor.size;
+      fonts = {
+        serif = {
+          package = cfg.fonts.serif.package;
+          name = cfg.fonts.serif.name;
         };
 
-        opacity = {
-          applications = cfg.opacity.applications;
-          terminal = cfg.opacity.terminal;
-        };
-        
-        icons = {
-          enable = true;
-
-          package = cfg.icons.package;
-          dark = cfg.icons.dark;
-          light = cfg.icons.light;
+        sansSerif = {
+          package = cfg.fonts.sansSerif.package;
+          name = cfg.fonts.sansSerif.name;
         };
 
-        fonts = {
-          serif = {
-            package = cfg.fonts.serif.package;
-            name = cfg.fonts.serif.name;
-          };
+        monospace = {
+          package = cfg.fonts.monospace.package;
+          name = cfg.fonts.monospace.name;
+        };
 
-          sansSerif = {
-            package = cfg.fonts.sansSerif.package;
-            name = cfg.fonts.sansSerif.name;
-          };
+        emoji = {
+          package = cfg.fonts.emoji.package;
+          name = cfg.fonts.emoji.name;
+        };
 
-          monospace = {
-            package = cfg.fonts.monospace.package;
-            name = cfg.fonts.monospace.name;
-          };
-
-          emoji = {
-            package = cfg.fonts.emoji.package;
-            name = cfg.fonts.emoji.name;
-          };
-
-          sizes = {
-            applications = cfg.fonts.sizes.applications;
-            desktop = cfg.fonts.sizes.desktop;
-            popups = cfg.fonts.sizes.popups;
-            terminal = cfg.fonts.sizes.terminal;
-          };
+        sizes = {
+          applications = cfg.fonts.sizes.applications;
+          desktop = cfg.fonts.sizes.desktop;
+          popups = cfg.fonts.sizes.popups;
+          terminal = cfg.fonts.sizes.terminal;
         };
       };
-    })
-  ];
+    };
+  };
 }
