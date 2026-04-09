a7e1e8f186f5d19b4e500610c310bad1dca5855e
Quirin Brändli <qbraendli@pm.me>
2026-04-09 03:18:01 +0200

    add polkit

--- profiles/nixos/desktop.nix
+++ profiles/nixos/desktop.nix
@@ -3,10 +3,17 @@ let
   sharedQnix = import ../shared/desktop.nix { inherit lib; };
 in
 {
-  imports = [ ./workstation.nix ];
+  imports = [
+    ./workstation.nix
+  ] ++ (lib.qnix.mkNixosFeatureImports {
+      category = "security";
+      name = "polkit";
+    });
 
   config = {
     qnix = lib.recursiveUpdate sharedQnix {
+      security.polkit.enable = lib.mkDefault true;
+
       system.packages.nerdFonts.enable = lib.mkDefault true;
 
       network.networkmanager = {
