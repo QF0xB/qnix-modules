d33d8559ddef9200ca100fba603de6e55ef36458
Quirin Brändli <qbraendli@pm.me>
2026-09-17 01:15:49 +0200

    fix(desktop): resolve PR notifier option duplication

--- profiles/hyprland.nix
+++ profiles/hyprland.nix
@@ -21,6 +21,7 @@
       "apps.file-manager"
       "apps.social"
       "desktop.clipboard"
+      "desktop.client-pr-notify"
       "desktop.hyprland"
       "desktop.hyprland.keybinds"
       "desktop.hyprland.monitors"
