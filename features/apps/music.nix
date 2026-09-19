a45cac0329d91d8e6e9e859cff025c232df807e3
Quirin Brändli <qbraendli@pm.me>
2026-09-20 00:36:08 +0200

    fix(apps): persist Tidal caches on cache dataset

--- features/apps/music.nix
+++ features/apps/music.nix
@@ -6,6 +6,15 @@
   ];
 
   persistence.users."*".directories = [ ".config/tidal-hifi" ];
+  persistence.users."*".cache.directories = [
+    ".config/tidal-hifi/Cache"
+    ".config/tidal-hifi/Code Cache"
+    ".config/tidal-hifi/Crashpad"
+    ".config/tidal-hifi/DawnGraphiteCache"
+    ".config/tidal-hifi/DawnWebGPUCache"
+    ".config/tidal-hifi/GPUCache"
+    ".config/tidal-hifi/component_crx_cache"
+  ];
 
   options =
     { lib, pkgs, ... }:
